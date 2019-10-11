8fa7cef279d54d9e1f6f84602825e13985686ce7
Kay Plößer <kay-is@users.noreply.github.com>
2019-10-12 00:19:08 +0200

    Add main script

---
+++ index.js
@@ -0,0 +1,22 @@
+  $("img").each(function() {
+    const element = $(this);
+    const throwableElement = element.clone();
+    element.css("visibility", "hidden");
+    throwableElement.insertBefore(element);
+    throwableElement.throwable({
+      bounce: 0.5,
+      damping: 50,
+      collisionDetection: false
+    });
+  });
+
+  document.body.style.cursor = `url("https://kay-is.github.io/undefined-gooselet/goose.png"), default`;
+  const honk = new Audio(
+    "https://kay-is.github.io/undefined-gooselet/honk.mp3"
+  );
+  document.oncontextmenu = () => {
+    honk.play();
+    return false;
+  };
+
+  document.oncontextmenu();
