0bf460926c52f450b776e8f00c9f52a77e95855c
Kay Plößer <kay-is@users.noreply.github.com>
2019-10-12 03:24:26 +0200

    Filter out invisible elements

--- index.js
+++ index.js
@@ -1,36 +1,46 @@
-function makeThrowable() {
-  const element = $(this);
-  const throwableElement = element.clone();
-  element.css("visibility", "hidden");
-  throwableElement.css("z-index", 2147483647);
-  throwableElement.insertBefore(element);
-  throwableElement.throwable({
-    bounce: 0.5,
-    damping: 50,
-    collisionDetection: false
-  });
+console.log(undefined, "gooselet v1 loaded!");
+
+function makeThrowable(element) {
+  const jqElement = $(element);
+  const clonedJqElement = jqElement.clone();
+  jqElement.css("visibility", "hidden");
+  clonedJqElement.css("z-index", 2147483647);
+  clonedJqElement.insertBefore(jqElement);
+  clonedJqElement.throwable({ bounce: 0.6, damping: 75 });
 }
 
-function getBackgroundDivs() {
-  const allDivs = document.querySelectorAll("div");
-  const backgroundDivs = [];
-  for (let div of allDivs) {
-    const { backgroundImage } = window.getComputedStyle(div, false);
-    if (backgroundImage.match(/url/)) backgroundDivs.push(div);
-  }
-  return backgroundDivs;
+function isVisible(element) {
+  const { display, visibility } = window.getComputedStyle(element, false);
+  if (display == "none" || visibility == "hidden") return false;
+  const { innerHeight, innerWidth } = window;
+  const { bottom, left, right, top } = element.getBoundingClientRect();
+  return top >= 0 && left >= 0 && bottom <= innerHeight && right <= innerWidth;
 }
 
-$(getBackgroundDivs()).each(makeThrowable);
-$("img").each(makeThrowable);
+function hasBackgroundImage(element) {
+  return window.getComputedStyle(div, false).backgroundImage.match(/url/);
+}
+
+function getValidDivs() {
+  const validDivs = [...document.querySelectorAll("div")]
+    .filter(isVisible)
+    .filter(hasBackgroundImage);
+  console.log(
+    "Visible <div>s with background-image found: " + validDivs.length
+  );
+  return validDivs;
+}
+
+function getValidImgs() {
+  const validImgs = [...document.querySelectorAll("img")].filter(isVisible);
+  console.log("Visible <img>s found: " + validImgs.length);
+  return validImgs;
+}
+
+[...getValidDivs(), ...getValidImgs()].forEach(makeThrowable);
 
 document.body.style.cursor = `url("https://kay-is.github.io/undefined-gooselet/goose.png"), default`;
-const honk = new Audio(
-  "https://kay-is.github.io/undefined-gooselet/honk.mp3"
-);
-document.oncontextmenu = () => {
-  honk.play();
-  return false;
-};
 
+const honk = new Audio("https://kay-is.github.io/undefined-gooselet/honk.mp3");
+document.oncontextmenu = () => honk.play();
 document.oncontextmenu();
