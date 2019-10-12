165c70d2ba0c2ce59d6c08f72796dcdd519b9c54
Kay Plößer <kay-is@users.noreply.github.com>
2019-10-12 02:12:57 +0200

    Add support for divs with background-image

--- index.js
+++ index.js
@@ -1,23 +1,36 @@
-  $("img").each(function() {
-    const element = $(this);
-    const throwableElement = element.clone();
-    element.css("visibility", "hidden");
-    throwableElement.css("z-index", 2147483647);
-    throwableElement.insertBefore(element);
-    throwableElement.throwable({
-      bounce: 0.5,
-      damping: 50,
-      collisionDetection: false
-    });
+function makeThrowable() {
+  const element = $(this);
+  const throwableElement = element.clone();
+  element.css("visibility", "hidden");
+  throwableElement.css("z-index", 2147483647);
+  throwableElement.insertBefore(element);
+  throwableElement.throwable({
+    bounce: 0.5,
+    damping: 50,
+    collisionDetection: false
   });
+}
 
-  document.body.style.cursor = `url("https://kay-is.github.io/undefined-gooselet/goose.png"), default`;
-  const honk = new Audio(
-    "https://kay-is.github.io/undefined-gooselet/honk.mp3"
-  );
-  document.oncontextmenu = () => {
-    honk.play();
-    return false;
-  };
+function getBackgroundDivs() {
+  const allDivs = document.querySelectorAll("div");
+  const backgroundDivs = [];
+  for (let div of allDivs) {
+    const { backgroundImage } = window.getComputedStyle(div, false);
+    if (backgroundImage.match(/url/)) backgroundDivs.push(div);
+  }
+  return backgroundDivs;
+}
 
-  document.oncontextmenu();
+$(getBackgroundDivs()).each(makeThrowable);
+$("img").each(makeThrowable);
+
+document.body.style.cursor = `url("https://kay-is.github.io/undefined-gooselet/goose.png"), default`;
+const honk = new Audio(
+  "https://kay-is.github.io/undefined-gooselet/honk.mp3"
+);
+document.oncontextmenu = () => {
+  honk.play();
+  return false;
+};
+
+document.oncontextmenu();
